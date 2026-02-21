05bf7912d518f9e82802cdea3beeb525ab8d5cb0
Jorge Roldan <roldanjrg@protonmail.com>
2026-02-21 16:33:34 -0500

    finished ch0_2/section2

---
+++ src/arena3/ch0_2/section2/section2_training_neural_networks.py
@@ -0,0 +1,251 @@
+# %%
+# Setup imports
+import json
+import sys
+from collections import namedtuple
+from dataclasses import dataclass
+from pathlib import Path
+
+import einops
+import numpy as np
+import torch as t
+import torch.nn as nn
+import torch.nn.functional as F
+import torchinfo
+from IPython.display import display
+from jaxtyping import Float, Int
+from PIL import Image
+from rich import print as rprint
+from rich.table import Table
+from torch import Tensor
+from torch.utils.data import DataLoader, Subset
+from torchvision import datasets, models, transforms
+from tqdm.notebook import tqdm
+
+from src.arena3.utils.plotly_utils import line
+from src.arena3.utils.device import device
+from src.arena3.ch0_2.section1.simple_mlp import SimpleMLP
+
+# %%
+# Setup MNIST helpers and dataloaders
+MNIST_TRANSFORM = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Normalize(0.1307, 0.3081),
+    ]
+)
+
+
+def get_mnist(trainset_size: int = 10_000, testset_size: int = 1_000) -> tuple[Subset, Subset]:
+    """Returns a subset of MNIST training data."""
+
+    # Get original datasets, which are downloaded to "./data" for future use
+    mnist_trainset = datasets.MNIST(
+        "./data", train=True, download=True, transform=MNIST_TRANSFORM
+    )
+    mnist_testset = datasets.MNIST(
+        "./data", train=False, download=True, transform=MNIST_TRANSFORM
+    )
+
+    # # Return a subset of the original datasets
+    mnist_trainset = Subset(mnist_trainset, indices=range(trainset_size))
+    mnist_testset = Subset(mnist_testset, indices=range(testset_size))
+
+    return mnist_trainset, mnist_testset
+
+
+# %%
+# Get dataloaders and test them
+mnist_trainset, mnist_testset = get_mnist()
+mnist_trainloader = DataLoader(mnist_trainset, batch_size=64, shuffle=True)
+mnist_testloader = DataLoader(mnist_testset, batch_size=64, shuffle=False)
+
+# Get the first batch of test data, by starting to iterate over `mnist_testloader`
+for img_batch, label_batch in mnist_testloader:
+    print(f"{img_batch.shape=}\n{label_batch.shape=}\n")
+    break
+
+# Get the first datapoint in the test set, by starting to iterate over `mnist_testset`
+for img, label in mnist_testset:
+    print(f"{img.shape=}\n{label=}\n")
+    break
+
+t.testing.assert_close(img, img_batch[0])
+assert label == label_batch[0].item()
+# %% 
+# Training loop
+model = SimpleMLP().to(device)
+
+batch_size = 128
+epochs = 3
+
+mnist_trainset, _ = get_mnist()
+mnist_trainloader = DataLoader(mnist_trainset, batch_size=batch_size, shuffle=True)
+
+optimizer = t.optim.Adam(model.parameters(), lr=1e-3)
+loss_list = []
+
+for epoch in range(epochs):
+    pbar = tqdm(mnist_trainloader)
+
+    for imgs, labels in pbar:
+        # Move data to device, perform forward pass
+        imgs, labels = imgs.to(device), labels.to(device)
+        logits = model(imgs)
+
+        # Calculate loss, perform backward pass
+        loss = F.cross_entropy(logits, labels)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        # Update logs & progress bar
+        loss_list.append(loss.item())
+        pbar.set_postfix(epoch=f"{epoch + 1}/{epochs}", loss=f"{loss:.3f}")
+
+line(
+    loss_list,
+    x_max=epochs * len(mnist_trainset),
+    labels={"x": "Examples seen", "y": "Cross entropy loss"},
+    title="SimpleMLP training on MNIST",
+    width=700,
+)
+# %%
+# Training loop with args dataclass
+@dataclass
+class SimpleMLPTrainingArgs:
+    """
+    Defining this class implicitly creates an __init__ method, which sets arguments as below, e.g.
+    self.batch_size=64. Any of these fields can also be overridden when you create an instance, e.g.
+    SimpleMLPTrainingArgs(batch_size=128).
+    """
+
+    batch_size: int = 64
+    epochs: int = 3
+    learning_rate: float = 1e-3
+
+
+# %%
+def train(args: SimpleMLPTrainingArgs) -> tuple[list[float], SimpleMLP]:
+    """
+    Trains & returns the model, using training parameters from the `args` object. Returns the model,
+    and loss list.
+    """
+    model = SimpleMLP().to(device)
+
+    mnist_trainset, _ = get_mnist()
+    mnist_trainloader = DataLoader(mnist_trainset, batch_size=args.batch_size, shuffle=True)
+
+    optimizer = t.optim.Adam(model.parameters(), lr=args.learning_rate)
+    loss_list = []
+
+    for epoch in range(args.epochs):
+        pbar = tqdm(mnist_trainloader)
+
+        for imgs, labels in pbar:
+            # Move data to device, perform forward pass
+            imgs, labels = imgs.to(device), labels.to(device)
+            logits = model(imgs)
+
+            # Calculate loss, perform backward pass
+            loss = F.cross_entropy(logits, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # Update logs & progress bar
+            loss_list.append(loss.item())
+            pbar.set_postfix(epoch=f"{epoch + 1}/{args.epochs}", loss=f"{loss:.3f}")
+
+    return loss_list, model
+
+
+args = SimpleMLPTrainingArgs()
+loss_list, model = train(args)
+
+line(
+    loss_list,
+    x_max=args.epochs * len(mnist_trainset),
+    labels={"x": "Examples seen", "y": "Cross entropy loss"},
+    title="SimpleMLP training on MNIST",
+    width=700,
+)
+
+
+# %%
+# Exercise - add a validation loop
+def train(args: SimpleMLPTrainingArgs) -> tuple[list[float], SimpleMLP]:
+    """
+    Trains & returns the model, using training parameters from the `args` object. Returns the model,
+    and loss list.
+    """
+    model = SimpleMLP().to(device)
+
+    mnist_trainset, mnist_testset = get_mnist()
+    mnist_trainloader = DataLoader(mnist_trainset, batch_size=args.batch_size, shuffle=True)
+    mnist_testloader = DataLoader(mnist_testset, batch_size=args.batch_size, shuffle=False)
+
+    optimizer = t.optim.Adam(model.parameters(), lr=args.learning_rate)
+    loss_list = []
+    accuracy_list = []
+
+    for epoch in range(args.epochs):
+        pbar = tqdm(mnist_trainloader)
+
+        for imgs, labels in pbar:
+            # Move data to device, perform forward pass
+            imgs, labels = imgs.to(device), labels.to(device)
+            logits = model(imgs)
+
+            # Calculate loss, perform backward pass
+            loss = F.cross_entropy(logits, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # Update logs & progress bar
+            loss_list.append(loss.item())
+            pbar.set_postfix(epoch=f"{epoch + 1}/{args.epochs}", loss=f"{loss:.3f}")
+
+        # Validation loop - JR
+        with t.inference_mode(): 
+            pbar = tqdm(mnist_testloader)
+            accuracy_epoch = []
+            for imgs, labels in pbar:
+                imgs, labels = imgs.to(device), labels.to(device)
+                logits = model(imgs)
+                accuracy = (logits.argmax(dim=-1) == labels).sum().item() / len(labels)
+                accuracy_epoch.append(accuracy)
+            accuracy_list.append(np.mean(accuracy_epoch))
+
+        # Validation loop - Reference
+        # num_correct_classifications = 0
+        # for imgs, labels in mnist_testloader:
+        #     # Move data to device, perform forward pass in inference mode
+        #     imgs, labels = imgs.to(device), labels.to(device)
+        #     with t.inference_mode():
+        #         logits = model(imgs)
+            
+        #     # Compute num correct by comparing argmaxed logits to true labels
+        #     predictions = t.argmax(logits, dim=-1)
+        #     num_correct_classifications += (predictions == labels).sum().item()
+        
+        # # Compute & log total accuracy
+        # accuracy = num_correct_classifications / len(mnist_testset)
+        # accuracy_list.append(accuracy)
+
+
+    return loss_list, accuracy_list, model
+
+args = SimpleMLPTrainingArgs()
+loss_list, accuracy_list, model = train(args)
+
+line(
+    y=[loss_list, [0.1] + accuracy_list],  # we start by assuming a uniform accuracy of 10%
+    use_secondary_yaxis=True,
+    x_max=args.epochs * len(mnist_trainset),
+    labels={"x": "Num examples seen", "y1": "Cross entropy loss", "y2": "Test Accuracy"},
+    title="SimpleMLP training on MNIST",
+    width=800,
+)
+# %%
