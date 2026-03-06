7be76de60015eea5b7c88ff8be5480e643892157
Jorge Roldan <roldanjrg@protonmail.com>
2026-03-06 23:48:53 +0000

    feat(ch1_1): finished src/arena3/ch1_2/section2

--- src/arena3/ch1_2/section2/section2_finding_induction_heads.py
+++ src/arena3/ch1_2/section2/section2_finding_induction_heads.py
@@ -1,10 +1,16 @@
 # %%
 # Setup imports
 import circuitsvis as cv
+from eindex import eindex  # type: ignore[import-untyped]
 from IPython.display import display
+from jaxtyping import Float, Int
 import torch as t
+from torch import Tensor
 from transformer_lens import ActivationCache, HookedTransformer, HookedTransformerConfig
 
+from src.arena3.ch1_2.plotly_utils import (
+    plot_loss_difference,
+)
 from src.arena3.utils.device import device
 
 # %%
@@ -134,5 +140,94 @@ print("Heads attending to current token  = ", ", ".join(current_attn_detector(ca
 print("Heads attending to previous token = ", ", ".join(prev_attn_detector(cache)))
 print("Heads attending to first token    = ", ", ".join(first_attn_detector(cache)))
 
+
+# %%
+# Exercise - plot per-token loss on repeated sequence
+def generate_repeated_tokens(
+    model: HookedTransformer, seq_len: int, batch_size: int = 1
+) -> Int[Tensor, "batch_size full_seq_len"]:
+    """
+    Generates a sequence of repeated random tokens
+
+    Outputs are:
+        rep_tokens: [batch_size, 1+2*seq_len]
+    """
+    t.manual_seed(0)  # for reproducibility
+    prefix = (t.ones(batch_size, 1) * model.tokenizer.bos_token_id).long()
+    rep_tokens_half = t.randint(0, model.cfg.d_vocab, (batch_size, seq_len), dtype=t.int64)
+    rep_tokens = t.cat([prefix, rep_tokens_half, rep_tokens_half], dim=-1).to(device)
+    return rep_tokens
+
+
+def run_and_cache_model_repeated_tokens(
+    model: HookedTransformer, seq_len: int, batch_size: int = 1
+) -> tuple[Tensor, Tensor, ActivationCache]:
+    """
+    Generates a sequence of repeated random tokens, and runs the model on it, returning (tokens,
+    logits, cache). This function should use the `generate_repeated_tokens` function above.
+
+    Outputs are:
+        rep_tokens: [batch_size, 1+2*seq_len]
+        rep_logits: [batch_size, 1+2*seq_len, d_vocab]
+        rep_cache: The cache of the model run on rep_tokens
+    """
+    rep_tokens = generate_repeated_tokens(model, seq_len=seq_len)
+    rep_logits, rep_cache = model.run_with_cache(rep_tokens)
+    return rep_tokens, rep_logits, rep_cache
+
+
+def get_log_probs(
+    logits: Float[Tensor, "batch posn d_vocab"], tokens: Int[Tensor, "batch posn"]
+) -> Float[Tensor, "batch posn-1"]:
+    logprobs = logits.log_softmax(dim=-1)
+    # We want to get logprobs[b, s, tokens[b, s+1]], in eindex syntax this looks like:
+    correct_logprobs = eindex(logprobs, tokens, "b s [b s+1]")
+    return correct_logprobs  # type: ignore[no-any-return]
+
+
+# %%
+seq_len = 50
+batch_size = 1
+(rep_tokens, rep_logits, rep_cache) = run_and_cache_model_repeated_tokens(
+    model, seq_len, batch_size
+)
+rep_cache.remove_batch_dim()
+rep_str = model.to_str_tokens(rep_tokens)
+model.reset_hooks()
+log_probs = get_log_probs(rep_logits, rep_tokens).squeeze()
+
+print(f"Performance on the first half: {log_probs[:seq_len].mean():.3f}")
+print(f"Performance on the second half: {log_probs[seq_len:].mean():.3f}")
+
+plot_loss_difference(log_probs, rep_str, seq_len)
+
+# %%
+# Display attention
+for layer in range(model.cfg.n_layers):
+    attention_pattern = rep_cache["pattern", layer]
+    display(cv.attention.attention_patterns(tokens=rep_str, attention=attention_pattern))
+
+
+# %%
+# Exercise - make an induction-head detector
+def induction_attn_detector(cache: ActivationCache) -> list[str]:
+    """
+    Returns a list e.g. ["0.2", "1.4", "1.9"] of "layer.head" which you judge to be induction heads
+
+    Remember - the tokens used to generate rep_cache are (bos_token, *rand_tokens, *rand_tokens)
+    """
+    attn_heads = []
+    for layer in range(model.cfg.n_layers):
+        for head in range(model.cfg.n_heads):
+            attention_pattern = cache["pattern", layer][head]
+            # take avg of (-seq_len+1)-offset elements
+            seq_len = (attention_pattern.shape[-1] - 1) // 2
+            score = attention_pattern.diagonal(-seq_len + 1).mean()
+            if score > 0.4:
+                attn_heads.append(f"{layer}.{head}")
+    return attn_heads
+
+
+print("Induction heads = ", ", ".join(induction_attn_detector(rep_cache)))
 
 # %%
